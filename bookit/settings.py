2fc203af253a1f3797195e8dac45005e76cc8ee3
defpunk <defpunk@users.noreply.github.com>
2017-10-12 14:50:04 +0100

    Update settings.py

--- bookit/settings.py
+++ bookit/settings.py
@@ -2,6 +2,6 @@
 
 BOT_NAME = 'sc_scripts_demo'
 SPIDER_MODULES = ['bookit.spiders']
-NEWSPIDER_MODULE = 'sc_scripts_demo.spiders'
-USER_AGENT = 'sc_scripts_demo (http://scrapinghub.com)'
+NEWSPIDER_MODULE = 'bookit.spiders'
+USER_AGENT = 'bookit (http://scrapinghub.com)'
 ROBOTSTXT_OBEY = True
