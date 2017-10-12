da0760fb428e38f764cab91b2ebf0d02966e11a5
defpunk <defpunk@users.noreply.github.com>
2017-10-12 15:07:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='project',
     version='1.0',
+    scripts=[],
     packages=find_packages(),
     entry_points={'scrapy': ['settings = bookit.settings']},
 )
